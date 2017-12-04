129e4fda802ad458701a83027b73b4c7548bfb28
Naresh Miriyala <naresh.miriyala@redenergy.com.au>
2017-12-04 14:51:40 +1100

    Working code

--- src/main/groovy/com/dellnaresh/MicroServices_Config_server/MicroServicesConfigServerApplication.groovy
+++ src/main/groovy/com/dellnaresh/MicroServices_Config_server/MicroServicesConfigServerApplication.groovy
@@ -2,8 +2,10 @@ package com.dellnaresh.MicroServices_Config_server
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
+@EnableConfigServer
 class MicroServicesConfigServerApplication {
 
 	static void main(String[] args) {
