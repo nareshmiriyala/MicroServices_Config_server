b15267a9ea05436aef045db4f176aa80d69bc706
Naresh Miriyala <naresh.miriyala@redenergy.com.au>
2017-12-04 14:36:13 +1100

    First commit

---
+++ src/main/groovy/com/dellnaresh/MicroServices_Config_server/MicroServicesConfigServerApplication.groovy
@@ -0,0 +1,12 @@
+package com.dellnaresh.MicroServices_Config_server
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MicroServicesConfigServerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run MicroServicesConfigServerApplication, args
+	}
+}
---
+++ src/test/groovy/com/dellnaresh/MicroServices_Config_server/MicroServicesConfigServerApplicationTests.groovy
@@ -0,0 +1,16 @@
+package com.dellnaresh.MicroServices_Config_server
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class MicroServicesConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
